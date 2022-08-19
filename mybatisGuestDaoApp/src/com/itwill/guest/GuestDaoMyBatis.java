9cc02e6dc38185c1d9fd8f7b2d1d64048383f5ad
ddZoebb <jjushine24@naver.com>
2022-08-19 18:01:54 +0900

    ..

--- mybatisGuestDaoApp/src/com/itwill/guest/GuestDaoMyBatis.java
+++ mybatisGuestDaoApp/src/com/itwill/guest/GuestDaoMyBatis.java
@@ -2,6 +2,11 @@ package com.itwill.guest;
 
 import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 /*
 이름             널?       유형             
 -------------- -------- -------------- 
@@ -15,35 +20,50 @@ GUEST_CONTENT  NOT NULL VARCHAR2(4000)
  */
 
 public class GuestDaoMyBatis {
-
+	private SqlSessionFactory sqlSessionFactory;
+	public static final String NAMESPACE = "com.itwill.guest.mapper.GuestMapper.";
+	
 	public GuestDaoMyBatis() {
+		try {
+			sqlSessionFactory = new SqlSessionFactoryBuilder()
+					.build(Resources.getResourceAsStream("mybatis-config.xml"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	public int insertGuest(Guest guest) throws Exception {
-
-		return 0;
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int rowCount=sqlSession.insert(NAMESPACE+"insertGuest",guest);
+		sqlSession.close();
+		return rowCount;
 	}
 
 	public Guest selectByNo(int no) throws Exception {
 		Guest guest = null;
-
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		sqlSession.close();
 		return guest;
 	}
 
 	public List<Guest> selectAll() throws Exception {
 		List<Guest> guestList = null;
-
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		sqlSession.close();
 		return guestList;
 	}
 
 	public int updateGuest(Guest guest) throws Exception {
-
+		SqlSession sqlSession = sqlSessionFactory.openSession(false);
+		sqlSession.commit();
+		sqlSession.close();
 		return 0;
 	}
 
 	public int deleteGuest(int no) throws Exception {
-
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		sqlSession.close();
 		return 0;
 	}
 }
--- mybatisGuestDaoApp/src/com/itwill/guest/GuestDaoMyBatisTestMain.java
+++ mybatisGuestDaoApp/src/com/itwill/guest/GuestDaoMyBatisTestMain.java
@@ -5,7 +5,10 @@ public class GuestDaoMyBatisTestMain {
 	public static void main(String[] args) throws Exception{
 		GuestDaoMyBatis guestDaoMyBatis=new GuestDaoMyBatis();
 		System.out.println("----selectAll---");
-		guestDaoMyBatis.selectAll();
+		//guestDaoMyBatis.selectAll();
+		
+		System.out.println("insert");
+		System.out.println(guestDaoMyBatis.insertGuest(new Guest(0,"인서트", null, "insert@naver.cim", "insert.co.kr","인서트", "테스트")));
 	}
 
 }
